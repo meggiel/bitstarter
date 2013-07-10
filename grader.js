7bc3a6f3ee64be779ff8adecf5c5383595d382be
Meggie Ladlow <unimeg@gmail.com>
2013-07-10 00:01:48 -0400

    changed checks and grader

--- grader.js
+++ grader.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -46,6 +47,7 @@ if (require.main == module) {
         clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html',
         clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <heroku URL>', 'Heroku URL')
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
